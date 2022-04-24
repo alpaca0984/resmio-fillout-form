65ab533789e0e488301b3e20521f4c4c2e35b0c6
Masataka Hirano <alpaca0984@gmail.com>
2022-04-24 21:24:53 +0200

    remove options

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,26 @@
-// Requires content.js
+const fieldNames = ["name", "email", "phone"];
+
+async function save() {
+  const entries = Array.from(document.body.querySelectorAll("input"))
+    .filter((elem) => fieldNames.includes(elem.name))
+    .map((elem) => [elem.name, elem.value]);
+  await chrome.storage.sync.set(Object.fromEntries(entries));
+}
+
+async function fillOut(fields = fieldNames) {
+  const data = await chrome.storage.sync.get(fields);
+  Object.entries(data).forEach(([name, value]) => {
+    const $elem = document.body.querySelector(`input[name='${name}']`);
+    if ($elem != null) {
+      $elem.value = value;
+    }
+  });
+}
 
 document.getElementById("fillInformation")
   .addEventListener("click", async () => {
+    await save();
+
     const [tab] = await chrome.tabs.query({
       active: true,
       currentWindow: true,
@@ -10,7 +29,12 @@ document.getElementById("fillInformation")
       target: { tabId: tab.id },
       // The `func` doesn't carry over any of the current execution context of the function.
       //  https://developer.chrome.com/docs/extensions/reference/scripting/#runtime-functions
-      func: fillOutInformation,
-      args: [fieldNames],
+      func: fillOut,
+      args: [fieldNames]
     });
   });
+
+document.addEventListener('DOMContentLoaded', async function () {
+  // Fill extension form
+  await fillOut();
+}, false);
